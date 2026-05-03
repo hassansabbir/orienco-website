d67027eb83a310e05645fec4e25dbf5f4b85b52b
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2026-05-03 17:05:36 +0600

    chore: allow all development origins and remove trailing whitespace in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.10.7.47'],
+  allowedDevOrigins: ["*"],
   images: {
     remotePatterns: [
       {
@@ -17,7 +17,6 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "fahim5004.naimulhassan.me",
       },
-
     ],
   },
 };
